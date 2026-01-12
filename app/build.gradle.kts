79df1cc0d4a34f4424a0cd02613ca562b1f58658
Rohit <rohitchauhan7084645838@gmail.com>
2026-01-12 08:11:14 +0530

    adding some screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization.plugin)
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android") version "2.57.1"
+    alias(libs.plugins.google.gms.google.services)
 
 }
 
@@ -54,6 +55,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.firebase.auth)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/rohit/whatsappclone/presentation/screens/SplashScreen.kt
+++ app/src/main/java/com/rohit/whatsappclone/presentation/screens/SplashScreen.kt
@@ -1,18 +1,69 @@
 package com.rohit.whatsappclone.presentation.screens
 
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateOffsetAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 import com.rohit.whatsappclone.R
+import com.rohit.whatsappclone.presentation.navigation.AuthRouts
+import com.rohit.whatsappclone.presentation.navigation.DashBoardRouts
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-        Image(painter = painterResource(id=R.drawable.), contentDescription = "AppImage")
+    val ofSetX = remember { Animatable(-300f) }
+    val size = remember { Animatable(200f) }
+    LaunchedEffect(Unit) {
+        ofSetX.animateTo(
+            targetValue = 0f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                easing = FastOutSlowInEasing
+            )
+        )
+        size.animateTo(
+            targetValue = 2000f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = FastOutSlowInEasing
+            )
+        )
+        navController.navigate(
+            if (FirebaseAuth.getInstance().currentUser != null) DashBoardRouts.Root else AuthRouts.SignUpScreen
+        ) {
+            popUpTo(0)
+        }
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.chatapp),
+            contentDescription = "AppImage",
+            modifier = Modifier
+                .size(size.value.dp)
+                .offset(x = ofSetX.value.dp)
+        )
     }
 }
